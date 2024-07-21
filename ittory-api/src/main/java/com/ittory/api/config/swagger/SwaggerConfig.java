7e1b0cb387085e517b8af43cd593d192114107d6
jamjamyy <cuy666@naver.com>
2024-07-21 13:40:32 +0900

    RL1M-80 feat: Swagger 설정

---
+++ ittory-api/src/main/java/com/ittory/api/config/swagger/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.ittory.api.config.swagger;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Ittory", description = "Ittory API 명세서", version = "v1")) // v1 VS 0.1
+//        contact = @Contact(name = "JamJamyy", email = "jamjamyy@kau.kr")
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+
+}
