5dfdc8bdb4d6226048c8a4698819efb6766afe9b
KJBig <kjgi73k@gmail.com>
2024-07-27 01:16:45 +0900

    RL1M-51 fix : H2 DB 접속 권한
    - H2 DB Path 변경.
    - Spring Security 설정 변경.

--- ittory-api/src/main/java/com/ittory/api/config/security/SecurityConfig.java
+++ ittory-api/src/main/java/com/ittory/api/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ittory.api.config.security;
 
+import static com.ittory.common.constant.DBConstant.H2_PATH;
+
 import com.ittory.api.config.security.filter.CustomAuthenticationEntryPoint;
 import com.ittory.api.config.security.filter.ExceptionHandlerFilter;
 import com.ittory.api.config.security.filter.JwtAuthenticationFilter;
@@ -29,11 +31,13 @@ public class SecurityConfig {
         http
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(header -> header.frameOptions(FrameOptionsConfig::sameOrigin))
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(H2_PATH).permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling((config) -> config.authenticationEntryPoint(customAuthenticationEntryPoint))
---
+++ ittory-common/src/main/java/com/ittory/common/constant/DBConstant.java
@@ -0,0 +1,7 @@
+package com.ittory.common.constant;
+
+public class DBConstant {
+
+    public static final String H2_PATH = "/h2-console/**";
+
+}
