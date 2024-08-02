ef5af3e1ff8eee1a41a5dc6a3d850aaf236b2c23
KJBig <kjgi73k@gmail.com>
2024-08-02 21:39:09 +0900

    RL1M-90 feat : ittory-socket 모듈 컴포넌트 스캔 설정
    - ittory-common 모듈 추가.

---
+++ ittory-socket/src/main/java/com/ittory/socket/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.ittory.socket.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.ittory.common"})
+public class AppConfig {
+}
