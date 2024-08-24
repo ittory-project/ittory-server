cc271a7066cf00868f91974164b1141e4887013b
KJBig <kjgi73k@gmail.com>
2024-08-24 18:53:41 +0900

    RL1M-106 feat : 편지를 받은 편지함에 저장하는 기능 추가
    - 선착순 1명이 받은 편지함에 편지를 저장하는 기능 추가.
    - synchronized로 동시성 제어.
        - 추후 오토스케일링 사용 시 다른 방법으로 변경될 예정.
    - 이미 저장한 유저가 있다면 LetterBoxAlreadyStoredException()로 예외 발생.

--- ittory-api/src/main/java/com/ittory/api/letter/controller/LetterController.java
+++ ittory-api/src/main/java/com/ittory/api/letter/controller/LetterController.java
@@ -2,10 +2,20 @@ package com.ittory.api.letter.controller;
 
 import com.ittory.api.letter.dto.LetterCreateRequest;
 import com.ittory.api.letter.dto.LetterCreateResponse;
+import com.ittory.api.letter.dto.LetterStorageStatusResponse;
 import com.ittory.api.letter.usecase.LetterCreateUseCase;
+import com.ittory.api.letter.usecase.LetterStorageStatusCheckUseCase;
+import com.ittory.api.letter.usecase.LetterStoreInLetterBoxUseCase;
+import com.ittory.common.annotation.CurrentMemberId;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/letter")
@@ -13,10 +23,27 @@ import org.springframework.web.bind.annotation.*;
 public class LetterController {
 
     private final LetterCreateUseCase letterCreateUseCase;
+    private final LetterStorageStatusCheckUseCase letterStorageStatusCheckUseCase;
+    private final LetterStoreInLetterBoxUseCase letterStoreInLetterBoxUseCase;
 
     @PostMapping
     public ResponseEntity<LetterCreateResponse> createLetter(@RequestBody LetterCreateRequest request) {
         LetterCreateResponse response = letterCreateUseCase.execute(request);
         return ResponseEntity.ok().body(response);
     }
+
+    @Operation(summary = "편지가 저장가능한지 확인", description = "isStored가 false면 저장가능.")
+    @GetMapping("/{letterId}/storage-status")
+    public ResponseEntity<LetterStorageStatusResponse> getLetterStorageStatus(@PathVariable Long letterId) {
+        LetterStorageStatusResponse response = letterStorageStatusCheckUseCase.execute(letterId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @Operation(summary = "편지를 보관함에 저장")
+    @PostMapping("/{letterId}/store")
+    public ResponseEntity<Void> getLetterStorageStatus(@CurrentMemberId Long memberId, @PathVariable Long letterId) {
+        letterStoreInLetterBoxUseCase.execute(memberId, letterId);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- ittory-domain/src/main/java/com/ittory/domain/member/repository/impl/LetterBoxRepositoryImpl.java
+++ ittory-domain/src/main/java/com/ittory/domain/member/repository/impl/LetterBoxRepositoryImpl.java
@@ -6,6 +6,7 @@ import static com.ittory.domain.member.enums.LetterBoxType.RECEIVE;
 import com.ittory.domain.member.domain.LetterBox;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -20,6 +21,17 @@ public class LetterBoxRepositoryImpl implements LetterBoxRepositoryCustom {
                         .and(letterBox.letterBoxType.eq(RECEIVE))
                 ).fetch();
         return result.size() > 0;
+    }
+
+    @Override
+    public Optional<LetterBox> findReceivedByLetterIdWithLock(Long letterId) {
+        LetterBox result = jpaQueryFactory.selectFrom(letterBox)
+                .where(letterBox.letter.id.eq(letterId)
+                        .and(letterBox.letterBoxType.eq(RECEIVE))
+                )
+//                .setLockMode(LockModeType.PESSIMISTIC_READ)
+                .fetchFirst();
+        return Optional.ofNullable(result);
     }
 
 }
