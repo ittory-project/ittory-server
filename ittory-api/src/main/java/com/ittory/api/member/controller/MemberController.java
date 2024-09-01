0a9157612adaad898a0006b857cc1b8b6f090089
KJBig <kjgi73k@gmail.com>
2024-09-01 22:42:14 +0900

    fix : 사용자 인증정보 얻는 방법
    - CurrentMemberId 어노테이션 사용으로 수정.

--- ittory-api/src/main/java/com/ittory/api/member/controller/MemberController.java
+++ ittory-api/src/main/java/com/ittory/api/member/controller/MemberController.java
@@ -9,12 +9,10 @@ import com.ittory.api.member.usecase.MemberParticipationReadUseCase;
 import com.ittory.api.member.usecase.MemberWithdrawUseCase;
 import com.ittory.api.member.usecase.ReceivedLetterUseCase;
 import com.ittory.common.annotation.CurrentMemberId;
-import com.ittory.domain.member.domain.Member;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,9 +32,8 @@ public class MemberController {
 
     @Operation(summary = "마이페이지 정보 조회", description = "사용자의 마이페이지 정보를 조회합니다.")
     @GetMapping("/mypage")
-    // @AuthenticationPrincipal -> 이 부분 체크
-    public ResponseEntity<MemberDetailResponse> getMyPage(@AuthenticationPrincipal Member member) {
-        MemberDetailResponse response = memberDetailReadUseCase.execute(member.getId());
+    public ResponseEntity<MemberDetailResponse> getMyPage(@CurrentMemberId Long memberId) {
+        MemberDetailResponse response = memberDetailReadUseCase.execute(memberId);
         return ResponseEntity.ok().body(response);
     }
 
