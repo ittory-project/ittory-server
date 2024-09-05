9227b8aa9c16484d7cbad849eeb6b7bba67a0ff9
KJBig <kjgi73k@gmail.com>
2024-09-05 09:34:36 +0900

    RL1M-131 feat : 사용자가 재방문 유저인지 확인하는 기능 테스트
    - 사용자가 재방문 유저인지 확인하는 기능을 위한 테스트 코드 추가.

--- ittory-domain/src/test/java/com/ittory/domain/member/service/MemberDomainServiceTest.java
+++ ittory-domain/src/test/java/com/ittory/domain/member/service/MemberDomainServiceTest.java
@@ -3,9 +3,14 @@ package com.ittory.domain.member.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.ittory.domain.letter.domain.Letter;
+import com.ittory.domain.letter.repository.LetterRepository;
+import com.ittory.domain.member.domain.LetterBox;
 import com.ittory.domain.member.domain.Member;
+import com.ittory.domain.member.enums.LetterBoxType;
 import com.ittory.domain.member.enums.MemberStatus;
 import com.ittory.domain.member.exception.MemberException.MemberNotFoundException;
+import com.ittory.domain.member.repository.LetterBoxRepository;
 import com.ittory.domain.member.repository.MemberRepository;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
@@ -24,8 +29,16 @@ public class MemberDomainServiceTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private LetterRepository letterRepository;
+
+    @Autowired
+    private LetterBoxRepository letterBoxRepository;
+
     @AfterEach
     void clean() {
+        letterBoxRepository.deleteAll();
+        letterRepository.deleteAll();
         memberRepository.deleteAll();
     }
 
@@ -152,6 +165,25 @@ public class MemberDomainServiceTest {
         assertThat(member).isNotNull();
         assertThat(member.getSocialId()).isNull();
         assertThat(member.getMemberStatus()).isEqualTo(MemberStatus.DELETED);
+    }
+
+    @DisplayName("재방문 유저인지 확인한다.")
+    @Test
+    void checkVisitedMemberTest() {
+        //given
+        Member member1 = memberRepository.save(Member.create(1L, "tester1", null));
+        Member member2 = memberRepository.save(Member.create(2L, "tester2", null));
+        Letter letter = letterRepository.save(Letter.builder().title("test_letter").build());
+        letterBoxRepository.save(LetterBox.create(member1, letter, LetterBoxType.PARTICIPATION));
+        letterBoxRepository.save(LetterBox.create(member2, letter, LetterBoxType.RECEIVE));
+
+        //when
+        Boolean member1VisitedStatus = memberDomainService.checkVisitedMember(member1.getId());
+        Boolean member2VisitedStatus = memberDomainService.checkVisitedMember(member2.getId());
+
+        //then
+        assertThat(member1VisitedStatus).isTrue();
+        assertThat(member2VisitedStatus).isFalse();
     }
 
 }
