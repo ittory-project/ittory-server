6562829f00c9edff734f4c669d7a73ddb1d11205
KJBig <kjgi73k@gmail.com>
2024-09-21 17:23:10 +0900

    RL1M-139 feat : Swagger 설명 추가

--- ittory-api/src/main/java/com/ittory/api/guestbook/controller/GuestbookController.java
+++ ittory-api/src/main/java/com/ittory/api/guestbook/controller/GuestbookController.java
@@ -5,6 +5,7 @@ import com.ittory.api.guestbook.dto.GuestbookResponse;
 import com.ittory.api.guestbook.dto.GuestbookSaveRequest;
 import com.ittory.api.guestbook.service.GuestbookAllReadUseCase;
 import com.ittory.api.guestbook.service.GuestbookCreateUseCase;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,14 @@ public class GuestbookController {
     private final GuestbookCreateUseCase guestbookCreateUseCase;
     private final GuestbookAllReadUseCase guestbookAllReadUseCase;
 
+    @Operation(summary = "방명록 저장", description = "방명록 DB Id 기준으로 Green -> Red -> Blue -> Yellow 순으로 색 지정")
     @PostMapping
     public ResponseEntity<GuestbookResponse> saveGuestbook(@RequestBody GuestbookSaveRequest guestbookSaveRequest) {
         GuestbookResponse response = guestbookCreateUseCase.execute(guestbookSaveRequest);
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(summary = "방명록 리스트 조회", description = "Pagination 적용, 생성일 기준으로 내림차순 조회")
     @GetMapping("/all")
     public ResponseEntity<GuestbookPageResponse> getAllGuestbook(Pageable pageable) {
         GuestbookPageResponse response = guestbookAllReadUseCase.execute(pageable);
