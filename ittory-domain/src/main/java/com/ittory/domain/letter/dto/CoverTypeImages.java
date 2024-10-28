830b26724543d2a65f850af95ad87d40e4668e65
KJBig <kjgi73k@gmail.com>
2024-10-28 15:28:09 +0900

    feat : CoverType 이미지 요소 추가.
    - listImageUrl
    - selectImageUrl
    - editImageUrl
    - confirmImageUrl
    - outputBackgroundImageUrl
    - loadingBackgroundImageUrl

--- ittory-domain/src/main/java/com/ittory/domain/letter/dto/CoverTypeImages.java
+++ ittory-domain/src/main/java/com/ittory/domain/letter/dto/CoverTypeImages.java
@@ -9,15 +9,22 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CoverTypeImages {
 
-    private String imageUrl;
-    private String simpleUrl;
-    private String backgroundUrl;
+    private String listImageUrl;
+    private String selectImageUrl;
+    private String editImageUrl;
+    private String confirmImageUrl;
+    private String outputBackgroundImageUrl;
+    private String loadingBackgroundImageUrl;
 
-    public static CoverTypeImages of(String imageUrl, String simpleUrl, String backgroundUrl) {
+    public static CoverTypeImages of(String listImageUrl, String selectImageUrl, String editImageUrl,
+                                     String confirmImageUrl, String outputBackgroundImageUrl, String loadingBackgroundImageUrl) {
         return CoverTypeImages.builder()
-                .imageUrl(imageUrl)
-                .simpleUrl(simpleUrl)
-                .backgroundUrl(backgroundUrl)
+                .listImageUrl(listImageUrl)
+                .selectImageUrl(selectImageUrl)
+                .editImageUrl(editImageUrl)
+                .confirmImageUrl(confirmImageUrl)
+                .outputBackgroundImageUrl(outputBackgroundImageUrl)
+                .loadingBackgroundImageUrl(loadingBackgroundImageUrl)
                 .build();
     }
 
