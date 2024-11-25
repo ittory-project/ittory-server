cbabd19dd52dc8bb491165f0b2454ce7e12084fc
KJBig <kjgi73k@gmail.com>
2024-11-25 16:09:48 +0900

    RL1M-190 feat : 편지 시작 메세지 기능 추가
    - 편지 시작 시 메세지를 보내는 소켓 기능 추가.

---
+++ ittory-socket/src/main/java/com/ittory/socket/controller/LetterProcessController.java
@@ -0,0 +1,28 @@
+package com.ittory.socket.controller;
+
+import com.ittory.socket.dto.EndResponse;
+import com.ittory.socket.dto.StartResponse;
+import com.ittory.socket.usecase.LetterEndUseCase;
+import com.ittory.socket.usecase.LetterStartUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LetterProcessController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    private final LetterStartUseCase letterStartUseCase;
+    private final LetterEndUseCase letterEndUseCase;
+
+    @MessageMapping("/letter/start/{letterId}")
+    public void startMember(@DestinationVariable Long letterId) {
+        StartResponse response = letterStartUseCase.execute(letterId);
+        String destination = "/topic/letter/" + letterId;
+        messagingTemplate.convertAndSend(destination, response);
+    }
+}
