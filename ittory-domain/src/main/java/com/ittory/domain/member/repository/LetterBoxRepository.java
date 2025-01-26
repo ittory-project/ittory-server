90972ae083db996c49aeab6438513a5b0d311e01
jamjamyy <jamjamyy@kau.kr>
2025-01-26 19:24:14 +0900

    QueryDSL 수정

--- ittory-domain/src/main/java/com/ittory/domain/member/repository/LetterBoxRepository.java
+++ ittory-domain/src/main/java/com/ittory/domain/member/repository/LetterBoxRepository.java
@@ -4,7 +4,7 @@ import com.ittory.domain.member.domain.LetterBox;
 import com.ittory.domain.member.enums.LetterBoxType;
 import com.ittory.domain.member.repository.impl.LetterBoxRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//
 public interface LetterBoxRepository extends JpaRepository<LetterBox, Long>, LetterBoxRepositoryCustom {
     void deleteByMemberIdAndLetterId(Long memberId, Long letterId);
 }
