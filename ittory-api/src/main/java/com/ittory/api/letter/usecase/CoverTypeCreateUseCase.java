fc610c48220da20c25bda3be98297325d80f2bdb
KIM JUNGI <101792740+KJBig@users.noreply.github.com>
2025-02-11 09:50:37 +0900

    RL1M-227 fix : Mapper -> Convertor
    - 클래스 이름 변경. Mapper -> Convertor

--- ittory-api/src/main/java/com/ittory/api/letter/usecase/CoverTypeCreateUseCase.java
+++ ittory-api/src/main/java/com/ittory/api/letter/usecase/CoverTypeCreateUseCase.java
@@ -1,8 +1,8 @@
 package com.ittory.api.letter.usecase;
 
+import com.ittory.api.letter.convertor.CoverTypeConvertor;
 import com.ittory.api.letter.dto.CoverTypeCreateRequest;
 import com.ittory.api.letter.dto.CoverTypeCreateResponse;
-import com.ittory.api.letter.mapper.CoverTypeMapper;
 import com.ittory.domain.letter.domain.CoverType;
 import com.ittory.domain.letter.dto.CoverTypeImages;
 import com.ittory.domain.letter.service.CoverTypeDomainService;
@@ -15,10 +15,10 @@ public class CoverTypeCreateUseCase {
 
     private final CoverTypeDomainService coverTypeDomainService;
 
-    private final CoverTypeMapper coverTypeMapper;
+    private final CoverTypeConvertor coverTypeConvertor;
 
     public CoverTypeCreateResponse execute(CoverTypeCreateRequest request) {
-        CoverTypeImages images = coverTypeMapper.toCoverTypeImages(request);
+        CoverTypeImages images = coverTypeConvertor.toCoverTypeImages(request);
         CoverType coverType = coverTypeDomainService.createCoverType(request.getName(), images);
         return CoverTypeCreateResponse.from(coverType);
     }
