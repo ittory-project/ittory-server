bdf93585b663bb821e23cf74014c4807bc68ab91
KIM JUNGI <101792740+KJBig@users.noreply.github.com>
2025-03-27 14:38:54 +0900

    RL1M-243 feat : 참여한 편지함에 편지 저장 기능 테스트
    - 관련 테스트 코드 추가.
    - LetterBoxControllerTest 추가.

--- ittory-api/src/test/java/com/ittory/api/member/controller/LetterBoxControllerTest.java
+++ ittory-api/src/test/java/com/ittory/api/member/controller/LetterBoxControllerTest.java
@@ -1,5 +1,7 @@
 package com.ittory.api.member.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ittory.api.member.dto.LetterBoxParticipationRequest;
 import com.ittory.api.member.service.LetterBoxService;
 import com.ittory.common.jwt.AccessTokenInfo;
 import com.ittory.common.jwt.JwtProvider;
@@ -12,12 +14,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MemberControllerTest {
+public class LetterBoxControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -28,25 +30,30 @@ public class MemberControllerTest {
     @MockBean
     private JwtProvider jwtProvider;
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     private static final String ACCESS_TOKEN = "access_token";
 
-    @DisplayName("참여자 편지함에서 편지 삭제")
+
+    @DisplayName("참여자 편지함에 편지 저장")
     @Test
-    public void deleteLetterInLetterBoxTest() throws Exception {
+    public void saveInParticipationLetterBoxTest() throws Exception {
         //given
-        Long letterId = 1L;
+        LetterBoxParticipationRequest request = new LetterBoxParticipationRequest(1L);
         AccessTokenInfo memberTokenInfo = AccessTokenInfo.of("1", "MEMBER");
 
         //when
         when(jwtProvider.resolveToken(ACCESS_TOKEN)).thenReturn(memberTokenInfo);
-        doNothing().when(letterBoxService).deleteLetterInLetterBox(1L, letterId);
+        doNothing().when(letterBoxService).saveInParticipationLetterBox(any(LetterBoxParticipationRequest.class));
 
         //then
-        mockMvc.perform(delete("/api/member/{letterId}", letterId)
-                        .header("Authorization", ACCESS_TOKEN))
+        mockMvc.perform(post("/api/letter-box/participation")
+                        .header("Authorization", ACCESS_TOKEN)
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(request))
+                )
                 .andExpect(status().isOk());
 
-        verify(letterBoxService).deleteLetterInLetterBox(1L, letterId);
+        verify(letterBoxService).saveInParticipationLetterBox(refEq(request));
 
 
     }
--- ittory-api/src/test/java/com/ittory/api/member/service/LetterBoxServiceTest.java
+++ ittory-api/src/test/java/com/ittory/api/member/service/LetterBoxServiceTest.java
@@ -1,7 +1,12 @@
 package com.ittory.api.member.service;
 
+import com.ittory.api.member.dto.LetterBoxParticipationRequest;
+import com.ittory.domain.letter.domain.Letter;
+import com.ittory.domain.letter.enums.LetterStatus;
 import com.ittory.domain.letter.service.LetterDomainService;
 import com.ittory.domain.member.service.LetterBoxDomainService;
+import com.ittory.domain.participant.domain.Participant;
+import com.ittory.domain.participant.enums.ParticipantStatus;
 import com.ittory.domain.participant.service.ParticipantDomainService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class LetterBoxServiceTest {
@@ -45,6 +54,30 @@ class LetterBoxServiceTest {
 
         // then
         verify(letterBoxDomainService).deleteByMemberIdAndLetterId(memberId, letterId);
+    }
+
+    @DisplayName("참여자 편지함에 편지 저장")
+    @Test
+    void saveInParticipationLetterBoxTest() {
+        // given
+        Long letterId = 1L;
+        LetterBoxParticipationRequest request = new LetterBoxParticipationRequest(letterId);
+        Letter letter = Letter.builder().id(letterId).build();
+        Participant participant1 = Participant.builder().participantStatus(ParticipantStatus.PROGRESS).build();
+        Participant participant2 = Participant.builder().participantStatus(ParticipantStatus.COMPLETED).build();
+        List<Participant> participants = Arrays.asList(participant1, participant2);
+
+        // when
+        when(letterDomainService.findLetter(letterId)).thenReturn(letter);
+        when(participantDomainService.findAllParticipants(letterId)).thenReturn(participants);
+
+        letterBoxService.saveInParticipationLetterBox(request);
+
+        // then
+        verify(letterBoxDomainService).saveAllInParticipationLetterBox(participants, letter);
+        verify(participantDomainService).updateAllStatusToEnd(letterId);
+        verify(participantDomainService).saveAllParticipant(participants);
+        verify(letterDomainService).updateLetterStatus(letterId, LetterStatus.COMPLETED);
     }
 
 }
