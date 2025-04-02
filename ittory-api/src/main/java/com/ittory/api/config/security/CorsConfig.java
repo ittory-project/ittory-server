cfcee13476c615b628e34e9624ab66eefd5e3bd2
KIM JUNGI <101792740+KJBig@users.noreply.github.com>
2025-04-02 17:29:48 +0900

    RL1M-257 feat : CORS 환경에서 localhost 주소 변경
    - http -> https
        - https://localhost:4173
        - https://localhost:5173

--- ittory-api/src/main/java/com/ittory/api/config/security/CorsConfig.java
+++ ittory-api/src/main/java/com/ittory/api/config/security/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
                 Arrays.asList("https://dev-server.ittory.co.kr", "http://localhost:8080",
-                        "https://ittory.co.kr", "http://localhost:4173", "http://localhost:5173",
+                        "https://ittory.co.kr", "https://localhost:4173", "https://localhost:5173",
                         "https://dev-client.ittory.co.kr"));
 
         configuration.addAllowedHeader("Authorization");
