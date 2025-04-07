bb2c8ecaf1fba270ab48223cd776993f45bb5c11
KIM JUNGI <101792740+KJBig@users.noreply.github.com>
2025-04-07 23:47:10 +0900

    RL1M-267 feat : socket 모듈 수정
    - WebSocketExceptionAdvice 캐치 시 디스코드 웹훅 발생.

--- ittory-socket/src/main/java/com/ittory/socket/common/exception/WebSocketExceptionAdvice.java
+++ ittory-socket/src/main/java/com/ittory/socket/common/exception/WebSocketExceptionAdvice.java
@@ -1,10 +1,10 @@
 package com.ittory.socket.common.exception;
 
 import com.ittory.common.exception.GlobalException;
+import com.ittory.infra.discord.WebHookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
@@ -15,24 +15,31 @@ public class WebSocketExceptionAdvice {
     private static final String LOG_FORMAT = "Error: {}, Class : {}, Message : {}";
     private static final String LOG_CODE_FORMAT = "Error: {}, Class : {}, Code : {}, Message : {}";
 
-    private final SimpMessagingTemplate messagingTemplate;
+    private final WebHookService webHookService;
 
     @MessageExceptionHandler(GlobalException.class)
-    public void handleCustomException(GlobalException exception) {
+    public void handleWebSocketGlobalException(GlobalException exception) {
         log.error(LOG_CODE_FORMAT, "GlobalException", exception.getClass().getName(),
                 exception.getErrorInfo().getCode(), exception.getMessage());
+        StringBuilder sb = new StringBuilder();
+        StringBuilder message = sb.append("======= Socket Error =======").append("\n")
+                .append(exception.getClass().getName())
+                .append(" : ")
+                .append(exception.getErrorInfo().getCode())
+                .append(" -> ")
+                .append(exception.getMessage());
+        webHookService.sendErrorLog(message.toString());
     }
 
-//    protected Message<byte[]> handleInternal(StompHeaderAccessor errorHeaderAccessor, byte[] errorPayload,
-//                                             @Nullable Throwable cause,
-//                                             @Nullable StompHeaderAccessor clientHeaderAccessor) {
-//
-//        return MessageBuilder.createMessage(errorPayload, errorHeaderAccessor.getMessageHeaders());
-//    }
-
     @MessageExceptionHandler(RuntimeException.class)
-    public void handleRuntimeException(Exception exception) {
+    public void handleWebSocketException(Exception exception) {
         log.error(LOG_FORMAT, "RuntimeException", exception.getClass().getName(), exception.getMessage());
+        StringBuilder sb = new StringBuilder();
+        StringBuilder message = sb.append("======= Socket Error =======").append("\n")
+                .append(exception.getClass().getName())
+                .append(" -> ")
+                .append(exception.getMessage());
+        webHookService.sendErrorLog(message.toString());
     }
 
 }
