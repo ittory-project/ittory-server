c8a1452b5c3087d1e8d7da7128d3991d0d369fd1
KIM JUNGI <101792740+KJBig@users.noreply.github.com>
2025-05-14 16:44:56 +0900

    RL1M-305 hotfix: 퇴장 Response 수정
    - 퇴장 Response 수정.
       - actionType -> action.

--- ittory-socket/src/main/java/com/ittory/socket/dto/ExitResponse.java
+++ ittory-socket/src/main/java/com/ittory/socket/dto/ExitResponse.java
@@ -4,8 +4,6 @@ import com.ittory.domain.participant.domain.Participant;
 import com.ittory.socket.enums.ActionType;
 import lombok.*;
 
-import java.util.List;
-
 import static com.ittory.socket.enums.ActionType.EXIT;
 
 @Getter
@@ -14,13 +12,13 @@ import static com.ittory.socket.enums.ActionType.EXIT;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExitResponse {
 
-    private ActionType actionType;
+    private ActionType action;
     private Long exitMemberId;
     private Boolean isManager;
 
     public static ExitResponse from(Participant participant, Boolean isManager) {
         return ExitResponse.builder()
-                .actionType(EXIT)
+                .action(EXIT)
                 .exitMemberId(participant.getMember().getId())
                 .isManager(isManager)
                 .build();
